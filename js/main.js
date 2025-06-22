6da24e8c664bc0c78abfe71c2a40a45a1f7c962c
Roman Panchuk <panchuk.roman98@gmail.com>
2025-06-22 21:40:04 +0200

    Fix and add new js markup

--- js/main.js
+++ js/main.js
@@ -4,30 +4,59 @@ const btn = document.querySelector(".btn");
 const addList = document.querySelector(".list");
 const deleteBtn = document.querySelector(".btn-reset");
 const itemList = document.querySelectorAll(".item-list");
+
 form.addEventListener("submit", (ev) => {
   ev.preventDefault();
+
   const toDo = input.value.trim();
+
+  if (toDo === "") {
+    return alert("Заповніть поле");
+  }
+
   const li = document.createElement("li");
   li.className = "item-list";
+
+  const textSpan = document.createElement("span");
+  textSpan.textContent = toDo;
+
   const check = document.createElement("input");
+  check.type = "checkbox";
+  check.className = "done-task";
+  check.name = "task";
+
   const delBtn = document.createElement("button");
   delBtn.type = "reset";
   delBtn.className = "btn-item";
   delBtn.textContent = "Видалити";
-  check.type = "checkbox";
-  check.className = "done-task";
-  check.name = "task";
-  if (toDo !== "") {
-    li.append(toDo + " ");
-    li.append(check);
-    li.append(delBtn);
-  } else return alert("Заповніть поле");
+
+  const div = document.createElement("div");
+  div.className = "box-div";
+  div.append(check);
+  div.append(delBtn);
+
+  check.addEventListener("change", () => {
+    if (check.checked) {
+      textSpan.style.textDecoration = "line-through";
+      textSpan.style.opacity = "0.6";
+    } else {
+      textSpan.style.textDecoration = "none";
+      textSpan.style.opacity = "1";
+    }
+  });
+
+  li.append(textSpan);
+  li.append(div);
+
   delBtn.addEventListener("click", () => {
     li.remove();
   });
-  deleteBtn.addEventListener("click", () => {
-    li.remove();
-  });
+
   addList.append(li);
   form.reset();
+});
+
+deleteBtn.addEventListener("click", (ev) => {
+  console.log(ev.target);
+  addList.innerHTML = "";
 });
